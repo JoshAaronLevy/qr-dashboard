c7fc82f173905fd14d6d542a553aa51728422868
Josh Levy <joshaaronlevy@gmail.com>
2021-11-06 15:57:40 -0600

    "Working on account form"

--- src/app/examples/accountsettings/accountsettings.component.ts
+++ src/app/examples/accountsettings/accountsettings.component.ts
@@ -1,50 +1,247 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
-import Choices from 'choices.js';
+import * as Parse from 'parse';
+import * as parse from "../../../keys/parse";
+import { getStoredUser, storeUser } from "src/shared/userHelper";
+import { parseResult, parseResults } from 'src/shared/parseResults';
+import { Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import swal from "sweetalert2";
 
 @Component({
-  selector: "app-accountsettings",
-  templateUrl: "accountsettings.component.html"
+	selector: "app-accountsettings",
+	templateUrl: "accountsettings.component.html"
 })
 export class AccountsettingsComponent implements OnInit, OnDestroy {
-  focus;
-  focus1;
-  focus2;
-  focus3;
-  focus4;
-  focus5;
-  constructor() {}
+	loading: boolean;
+	user: any;
+	originalUser: any;
+	emailChanged: boolean;
+	userId: string;
+	isAgent: boolean;
+	userEdit: FormGroup = new FormGroup({
+		firstName: new FormControl(''),
+		lastName: new FormControl(''),
+		email: new FormControl('')
+	});
+	agentEdit: FormGroup = new FormGroup({
+		firstName: new FormControl(''),
+		lastName: new FormControl(''),
+		mainPhone: new FormControl(''),
+		mobilePhone: new FormControl(''),
+		company: new FormControl(''),
+		website: new FormControl(''),
+		address1: new FormControl(''),
+		address2: new FormControl(''),
+		city: new FormControl(''),
+		state: new FormControl(''),
+		zip: new FormControl(''),
+		facebook: new FormControl(''),
+		linkedIn: new FormControl(''),
+		aboutMe: new FormControl('')
+	});
+	previousRoute: string;
+	username: string;
+	agent: any;
+	displayAgentForm: boolean;
+	displayUserForm: boolean;
+	firstName: any;
+	lastName: any;
+	email: any;
 
-  ngOnInit() {
-    var body = document.getElementsByTagName("body")[0];
-    body.classList.add("account-settings");
+	constructor(
+		private formBuilder: FormBuilder,
+		public router: Router
+	) {
+		Parse.initialize(parse.appId, parse.javascript);
+		Parse.serverURL = parse.serverURL;
+	}
 
-    new Choices('#choices-single-default', {
-      searchEnabled: false,
-    });
-    new Choices('#choices-single-default2', {
-      searchEnabled: false,
-    });
-    new Choices('#choices-single-default3', {
-      searchEnabled: false,
-    });
-    new Choices('#choices-single-default4', {
-      searchEnabled: false,
-    });
-    new Choices('#choices-single-default5', {
-      searchEnabled: false,
-    });
-    new Choices('#choices-single-default6', {
-      searchEnabled: false,
-    });
-    new Choices('#badges', {
-      delimiter: ',',
-      editItems: true,
-      maxItemCount: 5,
-      removeItemButton: true,
-    });
-  }
-  ngOnDestroy(){
-    var body = document.getElementsByTagName("body")[0];
-    body.classList.remove("account-settings");
-  }
+	ngOnInit() {
+		this.loading = true;
+		this.checkUser();
+		this.user = getStoredUser();
+		this.originalUser = { ...this.user };
+		if (this.originalUser.isAgent === 'YES') {
+			this.isAgent = true;
+		} else {
+			this.isAgent = false;
+		}
+		this.emailChanged = false;
+		const body = document.getElementsByTagName("body")[0];
+		body.classList.add("account-settings");
+	}
+
+	checkUser() {
+		const User = new Parse.User();
+		const query = new Parse.Query(User);
+		this.userId = getStoredUser().userId;
+		query.get(this.userId).then((user) => {
+			this.user = parseResult(user);
+			if (this.isAgent === true) {
+				this.checkAgent();
+			} else {
+				this.userForm();
+			}
+		}, (error) => {
+			this.presentUserError(error);
+		});
+	}
+
+	checkAgent() {
+		const Agents = Parse.Object.extend('Agents');
+		const query = new Parse.Query(Agents);
+		this.username = getStoredUser().username;
+		query.equalTo('agentID', this.username);
+		query.find().then((agent) => {
+			if (agent.length > 0) {
+				this.agent = parseResults(agent);
+				this.agent = this.agent[0];
+				localStorage.setItem('agentId', this.agent.id);
+				this.agentForm();
+			} else {
+				this.isAgent = false;
+			}
+		}, (error) => {
+			this.isAgent = false;
+			return error;
+			// this.presentUserError(error);
+		});
+	}
+
+	userForm() {
+		this.displayAgentForm = false;
+		this.displayUserForm = true;
+		this.userEdit = this.formBuilder.group({
+			firstName: this.user.firstName,
+			lastName: this.user.lastName,
+			email: this.user.email
+		});
+	}
+
+	agentForm() {
+		this.displayAgentForm = true;
+		this.displayUserForm = false;
+		const splitName = this.agent.agentDisplayName.split(' ');
+		const agentFirstName = splitName[0];
+		const agentLastName = splitName[1];
+		this.agentEdit = this.formBuilder.group({
+			firstName: agentFirstName,
+			lastName: agentLastName,
+			mainPhone: this.agent.agentPhoneMain,
+			mobilePhone: this.agent.agentPhoneMobile,
+			company: this.agent.agentOfficeName,
+			website: this.agent.website,
+			address1: this.agent.agentAddress,
+			address2: this.agent.agentAddress2,
+			city: this.agent.agentCity,
+			state: this.agent.agentState,
+			zip: this.agent.agentZip,
+			facebook: this.agent.facebook,
+			linkedIn: this.agent.linkedIn,
+			aboutMe: this.agent.agentAboutMe
+		});
+	}
+
+	evaluateEmail() {
+		if (this.userEdit.value.email !== this.originalUser.username) {
+			this.emailChanged = true;
+		} else {
+			this.emailChanged = false;
+		}
+	}
+
+	editProfile() {
+		if (this.isAgent === false) {
+			this.firstName = this.userEdit.value.firstName;
+			this.lastName = this.userEdit.value.lastName;
+			this.email = this.userEdit.value.email;
+			this.editUser();
+		} else {
+			this.firstName = this.agentEdit.value.firstName;
+			this.lastName = this.agentEdit.value.lastName;
+			this.email = this.userEdit.value.email;
+			this.editUser();
+			this.editAgent();
+		}
+	}
+
+	editUser() {
+		const User = new Parse.User();
+		const query = new Parse.Query(User);
+		query.get(this.userId).then((user) => {
+			const fullName = `${this.firstName} ${this.lastName}`;
+			user.set('firstName', this.firstName);
+			user.set('lastName', this.lastName);
+			user.set('fullname', fullName);
+			user.set('fullname_lower', fullName.toLowerCase());
+			user.save().then((response) => {
+				storeUser(response);
+				this.user = parseResult(response);
+			}).catch((error) => {
+				this.presentUserError(error);
+			});
+		});
+	}
+
+	editAgent() {
+		const Agents = Parse.Object.extend('Agents');
+		const query = new Parse.Query(Agents);
+		const agentId = localStorage.getItem('agentId');
+		query.get(agentId).then((agent) => {
+			const fullName = `${this.firstName} ${this.lastName}`;
+			agent.set('firstName', this.firstName);
+			agent.set('lastName', this.lastName);
+			agent.set('agentDisplayName', fullName);
+			agent.set('agentPhoneMain', this.agentEdit.value.mainPhone);
+			agent.set('agentPhoneMobile', this.agentEdit.value.mobilePhone);
+			agent.set('agentOfficeName', this.agentEdit.value.company);
+			agent.set('website', this.agentEdit.value.website);
+			agent.set('agentAddress', this.agentEdit.value.address1);
+			agent.set('agentAddress2', this.agentEdit.value.address2);
+			agent.set('agentCity', this.agentEdit.value.city);
+			agent.set('agentState', this.agentEdit.value.state);
+			agent.set('agentZip', this.agentEdit.value.zip);
+			agent.set('faceBook', this.agentEdit.value.facebook);
+			agent.set('linkedIn', this.agentEdit.value.linkedIn);
+			agent.set('agentAboutMe', this.agentEdit.value.aboutMe);
+			agent.save().then((response) => {
+				this.presentUserSuccess();
+				// this.router.navigate(['/settings']);
+				return response;
+			}).catch((error) => {
+				this.presentUserError(error);
+			});
+		});
+	}
+
+	presentUserSuccess() {
+		swal.fire({
+			title: "Success!",
+			timer: 1000,
+			showConfirmButton: false,
+			icon: "success"
+		});
+		setTimeout(() => {
+			this.loading = false;
+			this.router.navigate(['/dashboard']);
+		}, 1000);
+	}
+
+	presentUserError(error) {
+		swal.fire({
+			title: "Login Failed",
+			buttonsStyling: false,
+			customClass: {
+				confirmButton: "btn btn-danger",
+			},
+			icon: "error",
+			html: `<b>${error}</b>`
+		});
+		this.loading = false;
+	}
+
+	ngOnDestroy() {
+		const body = document.getElementsByTagName("body")[0];
+		body.classList.remove("account-settings");
+	}
 }
--- src/shared/userHelper.ts
+++ src/shared/userHelper.ts
@@ -5,12 +5,24 @@ export function storeUser(user: any) {
 		user = { ...user };
 	}
 	localStorage.setItem('userId', user.id);
-	localStorage.setItem('firstName', user.firstName);
-	localStorage.setItem('lastName', user.lastName);
-	localStorage.setItem('userId', user.id);
-	localStorage.setItem('isAgent', user.isagentyn);
-	localStorage.setItem('sessionToken', user.sessionToken);
-	localStorage.setItem('username', user.accountemail);
+	localStorage.setItem('firstName', user.firstName || "");
+	localStorage.setItem('lastName', user.lastName || "");
+	localStorage.setItem('userId', user.id || "");
+	localStorage.setItem('isAgent', user.isagentyn || "");
+	localStorage.setItem('sessionToken', user.sessionToken || "");
+	localStorage.setItem('username', user.accountemail || "");
+	localStorage.setItem('mainPhone', user.mainPhone || "");
+	localStorage.setItem('mobilePhone', user.mobilePhone || "");
+	localStorage.setItem('company', user.company || "");
+	localStorage.setItem('website', user.website || "");
+	localStorage.setItem('address1', user.address1 || "");
+	localStorage.setItem('address2', user.address2 || "");
+	localStorage.setItem('city', user.city || "");
+	localStorage.setItem('state', user.state || "");
+	localStorage.setItem('zip', user.zip || "");
+	localStorage.setItem('facebook', user.facebook || "");
+	localStorage.setItem('linkedIn', user.linkedIn || "");
+	localStorage.setItem('aboutMe', user.aboutMe || "");
 }
 
 export function getStoredUser() {
@@ -20,7 +32,19 @@ export function getStoredUser() {
 		lastName: localStorage.getItem('lastName'),
 		isAgent: localStorage.getItem('isAgent'),
 		sessionToken: localStorage.getItem('sessionToken'),
-		username: localStorage.getItem('username')
+		username: localStorage.getItem('username'),
+		mainPhone: localStorage.getItem('mainPhone'),
+		mobilePhone: localStorage.getItem('mobilePhone'),
+		company: localStorage.getItem('company'),
+		website: localStorage.getItem('website'),
+		address1: localStorage.getItem('address1'),
+		address2: localStorage.getItem('address2'),
+		city: localStorage.getItem('city'),
+		state: localStorage.getItem('state'),
+		zip: localStorage.getItem('zip'),
+		facebook: localStorage.getItem('facebook'),
+		linkedIn: localStorage.getItem('linkedIn'),
+		aboutMe: localStorage.getItem('aboutMe')
 	};
 }
 
